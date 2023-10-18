a258b1cfdc4b3970315ecbce3504a22e45c50de8
cheycattani <cheyennecattani@hotmail.com>
2023-10-18 12:32:08 -0300

    Desafio proposto na Introdução

--- 1_INTRO/hello-world/src/App.js
+++ 1_INTRO/hello-world/src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hello World React!</h1>
-    </div>
+    <><div className="App">
+      <h1>Hello World React!</h1>
+    </div><div className='Desafio'>
+        <p>Desafio 1</p>
+      </div></>
   );
 }
 
